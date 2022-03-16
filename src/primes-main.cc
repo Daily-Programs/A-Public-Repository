a42a074435ec38774b2cf847e840f0f1a1da38b0
cs-niu-programming <101152579+cs-niu-programming@users.noreply.github.com>
2022-03-16 12:09:53 -0500

    fixing

--- src/primes-main.cc
+++ src/primes-main.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-/*Mona added this to test the GitHub change notification within the Read the Docs and GradeScope
+/*Mona added this to test the GitHub change notification within the Read the Docs and GradeScope*/
 void primes(int limit);
 
 int main(int argc, char *argv[])
